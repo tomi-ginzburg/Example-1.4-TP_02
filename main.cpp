585432665947ff852bb1fb201ebd9f2bf478fdd8
tomi-ginzburg <tginzburg@fi.uba.ar>
2024-04-11 23:10:31 +0000

    final con BusIn y comentarios

--- main.cpp
+++ main.cpp
@@ -1,21 +1,38 @@
-#+include "mbed.h"
+#include "mbed.h"
 #include "arm_book_lib.h"
 
 /*
  * El programa se fija en cada ciclo el estado de los pines para determinar las acciones
  * si el pin D2(GAS) o D3(TEMPERATURA) estan en alto enciende la alarma(LED1)
  * Si se da la combinacion de los botones necesaria, se apaga la alarma
- * Para D2 y D3 puedo usar la clase PortIn
- * Para D4-D7 puedo usar la clase BusIn
+ * Voy a crear los objetos detectors y codeButton de la clase BusIn ya que D2-D3 y D4-D5-D6-D7 no comparten
+ * los mismos puertos entre si respectivamente
+ * Para BusIn puedo seleccionar con que pines puedo armar el bus de pines y no necesariamente tienen que ser del mismo puerto
  */
+
+
+void mostrarIO(int detectors, int buttons){
+    
+        printf("Gas/Temperatura: ");
+        switch(detectors){
+            case 0b00: printf("OFF/OFF\n"); break;
+            case 0b01: printf("ON/OFF\n"); break;
+            case 0b10: printf("OFF/ON\n"); break;
+            case 0b11: printf("ON/ON\n"); break;
+            default: printf("-\n");
+        }
+
+        printf("Codigo: %d\n",buttons);
+        
+}
 
 int main()
 {
-    /* Cambio los pines a D0 y D1 para que sean del mismo puerto (PORTG)
-     * DigitalIn gasDetector(D2);       Pasa al D0=PG_9
-     * DigitalIn overTempDetector(D3);  Pasa al D1=PG_14
+    /*
+     * DigitalIn gasDetector(D2);       
+     * DigitalIn overTempDetector(D3); 
      */
-    PortIn detectors(PortG,0x00004200) //0x00004200 seria un 1 en P14 y 1 en P9
+    BusIn detectors(D2,D3) ;
 
     /*
      * DigitalIn aButton(D4);
@@ -50,7 +67,7 @@ int main()
         /*
          * PortIn read() devuelve
          */
-        if ( detectors ) { // if ( gasDetector || overTempDetector ) {
+        if ( detectors == 0b01 || detectors == 0b10 ) { // if ( gasDetector || overTempDetector ) {
             alarmState = ON;
         }
 
@@ -61,8 +78,12 @@ int main()
          * de esa posicion. En este caso deberia tener un 1 en las posiciones primeras dos posiciones (D4 y D5)
          * y un 0 en las siguientes dos posiciones (D6 y D7) para que el codigo introducido sea correcto 
          */
-        if (codeButtons == 0011) { //if ( aButton && bButton && !cButton && !dButton) {
+        if (codeButtons == 0b0011) { //if ( aButton && bButton && !cButton && !dButton) {
             alarmState = OFF;
         }
+
+        // Muestro estado de salidas digitale y entradas digitales
+        mostrarIO(detectors.read(),codeButtons.read());
+        delay(50);
     }
 }
